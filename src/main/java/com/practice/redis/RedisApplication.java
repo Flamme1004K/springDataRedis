0c3e5b4a0ccfdc7f52ff368d4ff208edc2e8b7b9
flamme <flamme1004@gmail.com>
2021-02-08 23:46:40 +0900

    redis

--- src/main/java/com/practice/redis/RedisApplication.java
+++ src/main/java/com/practice/redis/RedisApplication.java
@@ -1,13 +1,13 @@
-package com.practice.reids;
+package com.practice.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReidsApplication {
+public class RedisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReidsApplication.class, args);
+        SpringApplication.run(RedisApplication.class, args);
     }
 
 }
--- src/test/java/com/practice/redis/ReidsApplicationTests.java
+++ src/test/java/com/practice/redis/ReidsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.practice.reids;
+package com.practice.redis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
