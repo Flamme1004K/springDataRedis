e7d1288b44ae744c8a1012a5572d7afd77a616d1
flamme <flamme1004@gmail.com>
2021-02-08 21:51:56 +0900

    First

---
+++ src/main/java/com/practice/reids/ReidsApplication.java
@@ -0,0 +1,13 @@
+package com.practice.reids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReidsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReidsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/reids/ReidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.reids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReidsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
