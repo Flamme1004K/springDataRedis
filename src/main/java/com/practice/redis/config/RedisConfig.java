fd1dbfb8496b9394ae907763c6d58dbf199acd92
flamme <flamme1004@gmail.com>
2021-02-26 21:39:38 +0900

    redis

---
+++ src/main/java/com/practice/redis/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.practice.redis.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory());
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .prefixKeysWith("imhere:")
+                .entryTtl(Duration.ofHours(5L));
+        builder.cacheDefaults(configuration);
+        return builder.build();
+    }
+
+    private RedisConnectionFactory redisConnectionFactory() {
+    }
+}
